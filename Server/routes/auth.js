82159cd5c8caf6293e3d2cf23b502e0c661b3cd1
Ajay1455 <75538701+Ajay1455@users.noreply.github.com>
2023-05-28 00:02:36 +0530

    test auth.js

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -24,7 +24,7 @@ router.post("/register", async (req, res) => {
 
 // Login work
 router.post("/login", async (req, res) => {
-  const val=User.find({});
+  const val=User.findOne({ email: req.body.email });
   console.log(val);
   try {
     const user = await User.findOne({ email: req.body.email });
