ba91c6861ba383594027f5734fa5ec0ea7773e20
Ajay1455 <75538701+Ajay1455@users.noreply.github.com>
2023-06-17 18:34:53 +0530

    error handling in Features.jsx

--- Client/src/components/features/Features.jsx
+++ Client/src/components/features/Features.jsx
@@ -1,38 +1,71 @@
 import { InfoOutlined, PlayArrow } from "@material-ui/icons";
 import "./Features.scss";
-import { useEffect, useState } from "react";
+import { useEffect,useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
-const Features = ({ type, setGenre}) => {
-  const [movie, setMovie] = useState({});
-  const Navigate=useNavigate();
+const Features = ({ type, setGenre }) => {
+  const [movie, setMovie] = useState({
+    _id: "642d87978e3a1a79fb27e226",
+    ageLimit: 16,
+    createdAt: "2023-04-05T14:37:11.355Z",
+    desc: "Annabelle is a 2014 American supernatural horror film directed by John R. Leonetti, written by Gary Dauberman and produced by Peter Safran and James Wan.",
+    genre: "horror",
+    img: "https://4.bp.blogspot.com/-lWEh25bE8GQ/XQsnN6Ot1aI/AAAAAAAAD_k/9TpYohKKnZcIVTqd4F9fWxYVnAik7NF6wCKgBGAs/w3840-h1600-c/annabelle-comes-home-uhdpaper.com-8K-1.jpg",
+    imgThumb:
+      "https://i0.wp.com/manapop.com/wp-content/uploads/2015/02/Annabelle-poster.jpg?ssl=1",
+    imgTitle: "https://www.pngmart.com/files/17/Annabelle-Logo-PNG-Picture.png",
+    isSeries: false,
+    title: "Annabella 2014",
+
+    trailer:
+      "https://www.shutterstock.com/video/clip-1054511090-traffic-increase-search-engine-optimization-boost-website",
+
+    video:
+      "https://www.shutterstock.com/video/clip-1052306503-flat-design-animation-digital-marketing-startup-improving",
+    year: "2014",
+  });
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     const getMovie = async () => {
       try {
         const res = await axios.get(`/movie/random?type=${type}`, {
           headers: {
             token:
-            "Bearer "+JSON.parse(localStorage.getItem("user")).accessToken,
+              "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
           },
         });
         setMovie(res.data[0]);
-        Navigate('/');
+        setIsLoading(false);
       } catch (error) {
-        console.log(error)
+        setError(error);
+        setIsLoading(false);
       }
     };
     getMovie();
   }, [type]);
 
-  console.log(movie)
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  console.log(movie);
 
   return (
     <div className="features">
       {type && (
         <div className="category">
-          <span>{type === "movies" ? "Movies" : "Series"}</span>
-          <select name="genre" id="genre" onChange={(e)=>setGenre(e.target.value)}>
+          <span>{type === "movie" ? "Movies" : "Series"}</span>
+          <select
+            name="genre"
+            id="genre"
+            onChange={(e) => setGenre(e.target.value)}
+          >
             <option>Genre</option>
             <option value="horror">Horror</option>
             <option value="comedy">Comedy</option>
@@ -44,29 +77,25 @@ const Features = ({ type, setGenre}) => {
           </select>
         </div>
       )}
-      <img
-        src={movie.img}
-        alt="Not found"
-        />
-      <div className="info">
-        <img
-          src={movie.imgTitle}
-          alt=""
-        />
-        <span className="desc">
-            {movie.desc}
-        </span>
-        <div className="buttons">
-          <button className="play">
-            <PlayArrow />
-            <span>Play</span>
-          </button>
-          <button className="more">
-            <InfoOutlined />
-            <span>Info</span>
-          </button>
-        </div>
-      </div>
+      {movie && (
+        <>
+          <img src={movie.img} alt="Not found" />
+          <div className="info">
+            <img src={movie.imgTitle} alt="" />
+            <span className="desc">{movie.desc}</span>
+            <div className="buttons">
+              <button className="play">
+                <PlayArrow />
+                <span>Play</span>
+              </button>
+              <button className="more">
+                <InfoOutlined />
+                <span>Info</span>
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
